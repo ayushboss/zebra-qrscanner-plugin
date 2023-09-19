1631dc3c9cb88b09092c0e1712b3156a0cc8701a
Francesco Bonacini <fbonnab@gmail.com>
2023-09-19 18:41:35 +0200

    refactor(src): update typescript exports

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,3 @@
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    QrScannerPlugin: QrScannerPluginPlugin;
-  }
-}
-
 export interface QrScannerPluginPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
 }
