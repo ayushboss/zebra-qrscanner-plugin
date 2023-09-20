02dbbadce82bfcf1fa75d696d076cc8fd6be50b0
Francesco Bonacini <fbonnab@gmail.com>
2023-09-20 12:38:28 +0200

    chore(package.json): bump dependencies, update scripts, update rollup

---
+++ README.md
@@ -0,0 +1,9 @@
+# QrScannerPluginPlugin
+
+This is the file used to generate documentation by docgen command.
+It needs to be placed here.
+
+## Plugin objective
+
+ This Capacitor plugin is intended to be used on Android only devices.\n
+ Its purpose is to make Android be able to connect to Zebra scanner devices.
--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,24 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'dist/esm/index.js',
-  output: {
+  output: [
+    {
     file: 'dist/plugin.js',
     format: 'iife',
-    name: 'capacitorPlugin', // TODO: change this
+    name: 'capacitorQrCodeScanner', // TODO: change this
     globals: {
       '@capacitor/core': 'capacitorExports',
     },
     sourcemap: true,
+    inlineDynamicImports: true,
   },
+  {
+    file: 'dist/plugin.cjs.js',
+    format: 'cjs',
+    sourcemap: true,
+    inlineDynamicImports: true,
+  },
+],
   plugins: [
     nodeResolve({
       // allowlist of dependencies to bundle in
@@ -18,4 +27,5 @@ export default {
       resolveOnly: ['lodash'],
     }),
   ],
+  external: ['@capacitor/core'],
 };
